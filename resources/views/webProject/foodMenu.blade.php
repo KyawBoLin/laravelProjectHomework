84746b6879aff99a1d54fb596bb6424c1f178569
KyawBoLin <kyawbobolin1995@gmail.com>
2021-11-17 11:24:22 +0630

    restaurant-website-insert-to-laravel

---
+++ resources/views/webProject/foodMenu.blade.php
@@ -0,0 +1,14 @@
+@extends('webProject.Layouts.master')
+
+@section('css')
+
+@endsection
+
+@section('content')
+    @include('webProject.Layouts.banner')
+    @include('webProject.Layouts.foodMenu')
+@endsection
+
+@section('script')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Controller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[Controller::class,'homePage']);
+
+Route::get('category',[Controller::class,'categoryPage']);
+
+Route::get('foodMenu',[Controller::class,'foodMenuPage'])->name('Food Menu');
